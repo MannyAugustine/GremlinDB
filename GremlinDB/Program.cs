f0ef940c07d72d12320e08b914abb56cf963bd8e
Manny Augustine <manny.augustine@live.com>
2018-02-02 04:36:09 -0800

    comm

--- GremlinDB/Program.cs
+++ GremlinDB/Program.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +11,23 @@ namespace GremlinDB
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task MainAsync(string[] args)
         {
+
+            string endpoint = ConfigurationManager.AppSettings["https://gremlindb.documents.azure.com:443/"];
+            string authKey = ConfigurationManager.AppSettings["1UyOhoGS7QQpFYOKy64jU98Bz5JvFrsJT9hTzFWRdNX8wuBfnccsQo3WLMnGclkuPRAOz0dt2ncE6D7YL5kVOQ=="];
+
+            DocumentClient client = new DocumentClient(new Uri(endpoint), authKey);
+
+            Database database = await client.CreateDatabaseIfNotExistsAsync(new Database { Id = "graphdb" });
+
+            DocumentCollection graph = await client.CreateDocumentCollectionIfNotExistsAsync(
+            UriFactory.CreateDatabaseUri("graphdb"),
+            new DocumentCollection { Id = "graphcollz" },
+            new RequestOptions { OfferThroughput = 1000 });
+
+
+
         }
     }
 }
